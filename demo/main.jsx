fe40226733ec05b94cc193eeef91f6426ab1e3f1
Akshita Singh <akshitasingh199as@gmail.com>
2025-07-03 12:45:13 +0530

    Fix createRoot import and build

--- demo/main.jsx
+++ demo/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../src/App"; 
+import { createRoot } from "react-dom/client"; 
+import App from "../src/App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
