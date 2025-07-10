9b8eeecc3e8b8af48047f61ec851a0b1e3cc2108
Akshita Singh <akshitasingh199as@gmail.com>
2025-07-10 13:50:26 +0530

    update fallback link behavior

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         deepLink="olx://"
         fallbackPlayStore="https://play.google.com/store/apps/details?id=com.olx.southasia"
         fallbackAppStore="https://apps.apple.com/in/app/olx-buy-sell-near-you/id913492792"
-        delay={1000}
+        delay={2500}
         autoRedirect={false}
         buttonText="Open In App"
       />
