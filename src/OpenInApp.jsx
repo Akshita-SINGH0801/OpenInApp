bb63d94b017be9deb1b91caec406cd6bafbb67f2
Akshita Singh <akshitasingh199as@gmail.com>
2025-07-15 13:34:21 +0530

    Updated OpenInApp component9

--- src/OpenInApp.jsx
+++ src/OpenInApp.jsx
@@ -7,7 +7,6 @@ const OpenInApp = ({
   delay = 2500,
   autoRedirect = false,
   buttonText = "Open in App",
-  androidPackage = "com.olx.southasia", 
 }) => {
   const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
   const isAndroid = /Android/i.test(navigator.userAgent);
@@ -15,29 +14,17 @@ const OpenInApp = ({
   const handleOpen = () => {
     if (!deepLink) return;
 
-    if (isIOS) {
-      
-      window.location.href = deepLink;
-      setTimeout(() => {
-        if (fallbackAppStore) {
-          window.location.href = fallbackAppStore;
-        }
-      }, delay);
-    } 
-   else if (isAndroid) {
-  const [schemePart, pathPartRaw] = deepLink.split('://');
-  const pathPart = pathPartRaw || '';
-  let intentUrl = `intent://${pathPart}#Intent;scheme=${schemePart};package=${androidPackage}`;
-  if (fallbackPlayStore) {
-    const fallbackParam = `;S.browser_fallback_url=${encodeURIComponent(fallbackPlayStore)}`;
-    intentUrl += fallbackParam;
-  }
-  intentUrl += ';end;';
-  window.location.href = intentUrl;
-}
-    else {
-      window.location.href = fallbackPlayStore || fallbackAppStore || '/';
-    }
+    // Go directly to the HTTPS deep link
+    window.location.href = deepLink;
+
+    // Optionally redirect to the store if nothing happens after delay
+    setTimeout(() => {
+      if (isIOS && fallbackAppStore) {
+        window.location.href = fallbackAppStore;
+      } else if (isAndroid && fallbackPlayStore) {
+        window.location.href = fallbackPlayStore;
+      }
+    }, delay);
   };
 
   useEffect(() => {
