7851de6ec14658a3abcad7875f87f188aa313591
Akshita Singh <akshitasingh199as@gmail.com>
2025-07-15 13:48:47 +0530

    Updated OpenInApp component12

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ function App() {
     <div className="app-container">
       <h1>Open In App</h1>
       <OpenInApp
-        deepLink="https://www.olx.in/"
+        deepLink="olx://home"
         fallbackPlayStore="https://play.google.com/store/apps/details?id=com.olx.southasia"
         fallbackAppStore="https://apps.apple.com/in/app/olx-buy-sell-near-you/id913492792"
+        androidPackage="com.olx.southasia"
         delay={2500}
         autoRedirect={false}
         buttonText="Open in OLX App"
